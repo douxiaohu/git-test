6a5ecaf77bd294907ef17318e8a200a338c5074c
douxiaohu <444701927@qq.com>
2022-08-27 09:57:47 +0800

    hutools commit 1

--- src/main/java/com/douxiaohu/test/test.java
+++ src/main/java/com/douxiaohu/test/test.java
@@ -2,15 +2,6 @@ package com.douxiaohu.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("hello git!!!!");
-        System.out.println("hi!");
-        System.out.println("test4");
-        System.out.println("test4");
-        System.out.println("test4");
-        System.out.println("test4");
-        System.out.println("test4");
-        System.out.println("尝试冲突合并");
-        System.out.println("t");
+        System.out.println("git lab");
     }
 }
