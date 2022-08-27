262a99a140ac4f9647682c7b7e002f6905610a11
douxiaohu <444701927@qq.com>
2022-08-27 15:00:57 +0800

    commit 3

--- src/main/java/com/douxiaohu/test/test.java
+++ src/main/java/com/douxiaohu/test/test.java
@@ -3,17 +3,5 @@ package com.douxiaohu.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello git!!!!");
-        System.out.println("hello github!!!!");
-        System.out.println("hello giter!!!!");
-        System.out.println("张磊好帅");
-        System.out.println("哈哈哈哈哈啊哈哈哈哈哈");
-        System.out.println("分支1");
-        System.out.println("git lab");
-        System.out.println("hello git!!!!");
-        System.out.println("hello github!!!!");
-        System.out.println("hello giter!!!!");
-        System.out.println("张磊好帅");
-        System.out.println("哈哈哈哈哈啊哈哈哈哈哈");
-        System.out.println("嘻嘻");
     }
 }
