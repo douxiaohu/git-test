ea6bb441ce1502796002014dc8822bcb2ff94106
douxiaohu <444701927@qq.com>
2022-08-27 14:41:03 +0800

    commit 1

--- src/main/java/com/douxiaohu/test/test.java
+++ src/main/java/com/douxiaohu/test/test.java
@@ -5,10 +5,5 @@ public class test {
         System.out.println("hello git!!!!");
         System.out.println("hello github!!!!");
         System.out.println("hello giter!!!!");
-        System.out.println("git lab");
-        System.out.println("git clone");
-        System.out.println("git clone");
-        System.out.println("git clone");
-        System.out.println("git pull");
     }
 }
